96686f489e2118ecb3c089d2e14772166ea193b3
Akhil <akhilf@juniper.net>
2018-02-13 14:56:59 +0530

    Added the floating IP Pools page
    Change-Id: Id928b93b21417babd1b0fb56317b8c2cc956287c
    Closes-Bug: #1732526

---
+++ webroot/config/networking/fippool/ui/js/views/fipPoolCfgEditView.js
@@ -0,0 +1,249 @@
+/*
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'backbone',
+    'contrail-list-model',
+    'knockback',
+    'contrail-view',
+    'config/networking/fippool/ui/js/views/fipPoolFormatters',
+], function (_, Backbone, ContrailListModel, Knockback, ContrailView,
+        FipPoolFormatter) {
+    var fipPoolFormatter = new FipPoolFormatter();
+    var prefixId = ctwc.FIP_POOL_PREFIX_ID,
+        modalId = 'configure-' + prefixId,
+        editTemplate = contrail.getTemplate4Id(cowc.TMPL_GENERIC_EDIT_FORM),
+        self = {};
+
+    var fipPoolCfgEditView = ContrailView.extend({
+        modalElementId: '#' + modalId,
+        renderFipPoolPopup: function (options) {
+            console.log("fipPool Edit",options);
+            var editLayout = editTemplate(
+                                {modalId: modalId, prefixId: prefixId}),
+                disableElement = false;
+            self = this;
+            cowu.createModal({'modalId': modalId,
+                              'className': 'modal-700',
+                              'title': options['title'],
+                              'body': editLayout,
+                              'onSave': function () {
+                self.model.configurePorts(options['mode'],
+                {
+                    init: function () {
+                        self.model.showErrorAttr(prefixId + cowc.FORM_SUFFIX_ID,
+                                                 false);
+                        cowu.enableModalLoading(modalId);
+                    },
+                    success: function () {
+                        options['callback']();
+                        $("#" + modalId).modal('hide');
+                    },
+                    error: function (error) {
+                        cowu.disableModalLoading(modalId, function () {
+                            self.model.showErrorAttr(
+                                       prefixId + cowc.FORM_SUFFIX_ID,
+                                       error.responseText);
+                        });
+                    }
+                });
+            }, 'onCancel': function () {
+                Knockback.release(self.model, document.getElementById(modalId));
+                kbValidation.unbind(self);
+                $("#" + modalId).modal('hide');
+            }});
+
+            if(options['mode'] == ctwl.EDIT_ACTION) {
+                disableElement = true;
+            }
+            self.renderView4Config(
+                $("#" + modalId).find("#" + modalId + "-form"),
+                self.model, self.getConfigureViewConfig(
+                     disableElement, options['mode'], options),
+                'portValidations', null, null,function(){
+                    self.model.showErrorAttr(prefixId +
+                                    cowc.FORM_SUFFIX_ID, false);
+                    Knockback.applyBindings(self.model,
+                                    document.getElementById(modalId));
+                    kbValidation.bind(self,
+                        {collection:
+                          self.model.model().attributes.fixedIPCollection}
+                        );
+                    kbValidation.bind(self,
+                        {collection:
+                          self.model.model().attributes.dhcpOptionCollection}
+                        );
+                    kbValidation.bind(self,
+                        {collection:
+                          self.model.model().attributes.allowedAddressPairCollection}
+                        );
+                    kbValidation.bind(self,
+                        {collection:
+                          self.model.model().attributes.fatFlowCollection}
+                        );
+                    kbValidation.bind(self,
+                        {collection:
+                          self.model.model().attributes.portBindingCollection}
+                        );
+                    //permissions
+                   ctwu.bindPermissionsValidation(self);
+                }, null, true, false
+            );
+            return;
+        },
+        renderDeleteFloatingIPPools: function (options) {
+            var delTemplate =
+                contrail.getTemplate4Id('core-generic-delete-form-template');
+            var self = this;
+
+            var delLayout = delTemplate({prefixId: prefixId});
+            cowu.createModal({'modalId': modalId, 'className': 'modal-480',
+                             'title': options['title'], 'btnName': 'Confirm',
+                             'body': delLayout,
+               'onSave': function () {
+                self.model.deleteFloatingIpPools(options['selectedGridData'], {
+                    init: function () {
+                        cowu.enableModalLoading(modalId);
+                    },
+                    success: function () {
+                        options['callback']();
+                        $("#" + modalId).modal('hide');
+                    },
+                    error: function (error) {
+                        cowu.disableModalLoading(modalId, function () {
+                            self.model.showErrorAttr(prefixId +
+                                                     cowc.FORM_SUFFIX_ID,
+                                                     error.responseText);
+                        });
+                    }
+                });
+            }, 'onCancel': function () {
+                Knockback.release(self.model, document.getElementById(modalId));
+                kbValidation.unbind(self);
+                $("#" + modalId).modal('hide');
+            }});
+            self.model.showErrorAttr(prefixId + cowc.FORM_SUFFIX_ID, false);
+            Knockback.applyBindings(self.model,
+                document.getElementById(modalId));
+            kbValidation.bind(self);
+        },
+        renderDeleteAllPort: function (options) {
+            var elId = 'deleteAllPortID';
+            var delTemplate =
+                contrail.getTemplate4Id(ctwl.TMPL_CORE_GENERIC_DEL),
+                self = this;
+            var delLayout = delTemplate({prefixId: prefixId,
+                                        item: "all port(s)",
+                                        itemId:""})
+            cowu.createModal({'modalId': modalId, 'className': 'modal-700',
+                             'title': options['title'], 'btnName': 'Confirm',
+                             'body': delLayout, 'onSave': function () {
+                self.model.deleteAllPort(options.selectedProjectId, {
+                    init: function () {
+                        self.model.showErrorAttr(prefixId + cowc.FORM_SUFFIX_ID, false);
+                        cowu.enableModalLoading(modalId);
+                    },
+                    success: function () {
+                        options['callback']();
+                        $("#" + modalId).modal('hide');
+                    },
+                    error: function (error) {
+                        cowu.disableModalLoading(modalId, function () {
+                            self.model.showErrorAttr(prefixId + cowc.FORM_SUFFIX_ID,
+                                                     error.responseText);
+                        });
+                    }
+                });
+            }, 'onCancel': function () {
+                Knockback.release(self.model, document.getElementById(modalId));
+                kbValidation.unbind(self);
+                $("#" + modalId).modal('hide');
+            }});
+            this.model.showErrorAttr(prefixId + cowc.FORM_SUFFIX_ID, false);
+            Knockback.applyBindings(this.model,
+                                    document.getElementById(modalId));
+            kbValidation.bind(this);
+        },
+        getConfigureViewConfig : function(isDisable, mode, options) {
+            return {
+                elementId: cowu.formatElementId([prefixId, ctwl.TITLE_EDIT_PORT]),
+                view: "SectionView",
+                title: "Floating IP Pools",//permissions
+               // active:false,
+                viewConfig: {
+                    rows: [
+                        {
+                            columns: [
+                                {
+                                    elementId: 'virtualNetworkName',
+                                    view: "FormDropdownView",
+                                    name: "Networks",
+                                    viewConfig: {
+                                        label: 'Networks',
+                                        disabled: isDisable,
+                                        path: 'virtualNetworkName',
+                                        dataBindValue: 'virtualNetworkName',
+                                        dropdownAutoWidth : false,
+                                        class: "col-xs-10",
+                                        elementConfig:{
+                                            allowClear: true,
+                                            dataTextField: "text",
+                                            dataValueField: "value",
+                                            defaultValueId : 0,
+                                            dataSource : {
+                                                type: "remote",
+                                                requestType: "GET",
+                                                url: ctwc.get(
+                                                        ctwc.URL_All_NETWORK_IN_PROJECT,
+                                                        options.selectedProjectId),
+                                                parse: function(result) {
+                                                    return fipPoolFormatter.formatNetworksData(self,
+                                                        result, mode);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            columns: [{
+                                elementId: 'pool_name',
+                                name: "Pool Name",
+                                view: "FormInputView",
+                                viewConfig: {
+                                    disabled: isDisable,
+                                    path: 'name',
+                                    label: 'Pool Name',
+                                    placeholder: 'Port Name',
+                                    dataBindValue: 'name',
+                                    class: "col-xs-10"
+                                }
+                            }]
+                        },
+                        {
+                            columns: [{
+                                elementId: 'pool_description',
+                                name: "Pool Description",
+                                view: "FormTextAreaView",
+                                viewConfig: {
+                                    disabled: false,
+                                    path: 'description',
+                                    label: 'Pool Description',
+                                    placeholder: 'Pool Description',
+                                    dataBindValue: 'description',
+                                    rows:"1",
+                                    cols:"100%",
+                                    class: "col-xs-10"
+                                }
+                            }]
+                        }
+                    ]
+                }
+            };
+        }
+    });
+    return fipPoolCfgEditView;
+});
